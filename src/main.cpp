bb297a6c2ee9052bb38584d467fbe598f55b5536
theWaffler <gerald@unnecessarilylongstringinput.io>
2024-10-23 13:31:30 -0700

    added music

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "raylib.h"
+#include <cstddef>
 
 // Global Variables
 const int screenWidth = 800;
 const int screenHeight = 450;
 Texture2D grassSprite;
+bool musicPaused = false;
+Music music;
 
 // Define custom colors
 struct Colors {
@@ -46,23 +49,24 @@ public:
 
   // WASD keyboard input
   void update(float deltaTime) {
-      if (IsKeyDown(KEY_D)) playerDest.x += speed * deltaTime;
-      if (IsKeyDown(KEY_A))  playerDest.x -= speed * deltaTime;
-      if (IsKeyDown(KEY_W))    playerDest.y -= speed * deltaTime;
-      if (IsKeyDown(KEY_S))  playerDest.y += speed * deltaTime;
+    if (IsKeyDown(KEY_D)) playerDest.x += speed * deltaTime;
+    if (IsKeyDown(KEY_A))  playerDest.x -= speed * deltaTime;
+    if (IsKeyDown(KEY_W))    playerDest.y -= speed * deltaTime;
+    if (IsKeyDown(KEY_S))  playerDest.y += speed * deltaTime;
+    if (IsKeyPressed(KEY_Q)) musicPaused = !musicPaused;
   }
 
   // draw player on the screen
   void draw() const {
-      DrawTexturePro(
-          playerTexture,   
-          playerSrc,       
-          playerDest,      
-          origin,          
-          0.0f,            
-          WHITE            
-      );
-    }
+    DrawTexturePro(
+        playerTexture,   
+        playerSrc,       
+        playerDest,      
+        origin,          
+        0.0f,            
+        WHITE            
+    );
+  }
 };
 
 // Initialize static member outside the class
@@ -72,6 +76,20 @@ const char* Player::texturePath = "../assets/Characters/BasicCharakterSpriteshee
 void drawScene() {};
 void updateGameLoop() {};
 
+bool initMusic() {
+  InitAudioDevice ();  // Initialize audio
+  music = LoadMusicStream("../assets/music/farm.mp3");  // Load music file
+  musicPaused = false;
+
+  //check
+  if (music.stream.buffer == NULL || music.ctxData == NULL) {
+    TraceLog(LOG_ERROR, "Music could not be loaded");
+    return false;
+  }
+  PlayMusicStream(music);
+  return true;
+}
+
 void initGame() {
   InitWindow(screenWidth, screenHeight, "I DUNNO WUT IM DOING");
   SetExitKey(0);  // Press X to close game
@@ -80,9 +98,15 @@ void initGame() {
   // Load the grass texture
   grassSprite = LoadTexture("../assets/Tilesets/Grass.png");
   if (grassSprite.id == 0) {
-      TraceLog(LOG_ERROR, "Grass texture could not be loaded");
+    TraceLog(LOG_ERROR, "Grass texture could not be loaded");
+  }
+
+  if (!initMusic()) {
+    TraceLog(LOG_ERROR, "Music could not be loaded");
   }
 }
+
+
 
 void renderGame(Player& player) {
   BeginDrawing();
@@ -96,13 +120,23 @@ void renderGame(Player& player) {
   // Draw the player
   player.draw();
 
+  //play and pause music
+  UpdateMusicStream(music);
+  if (musicPaused) {
+    PauseMusicStream(music);
+  } else {
+    ResumeMusicStream(music);
+  }
+
   drawScene();
   EndDrawing();
 }
 
 void closeGame() {
   UnloadTexture(grassSprite);  // Unload the grass texture
-  CloseWindow();               // Close the window
+  UnloadMusicStream(music);    // Unload the music stream
+  CloseAudioDevice();          // Close the audio device
+  CloseWindow();               // Close the stream
 }
 
 int main() {
